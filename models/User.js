356628d22f1b02f2091950c33c1db593d5ca367c
b00db <48580026+b00db@users.noreply.github.com>
2021-11-03 07:31:27 +0900

    logout

--- models/User.js
+++ models/User.js
@@ -73,7 +73,7 @@ userSchema.statics.findByToken = function(token, cb) {
     var user = this;
 
     jwt.verify(token, 'secretToken', function (err, decoded) {
-        user.findOne({"_id": jwt.decode, "token": token}, function (err, user) {
+        user.findOne({"_id": decoded, "token": token}, function (err, user) {
             if(err) return cb(err);
             cb(null, user);
         });
