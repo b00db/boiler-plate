c55fd74cb629b74da1c2da5c0fbfc14cc3a19ac0
b00db <48580026+b00db@users.noreply.github.com>
2021-11-03 03:32:09 +0900

    Generate Token

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const config = require('./config/key');
 
@@ -10,6 +11,8 @@ const { User } = require("./models/User");
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(bodyParser.json());
+
+app.use(cookieParser());
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI)
@@ -23,12 +26,36 @@ app.get('/', (req, res) => {
 app.post('/register', (req, res) => {
   const user = new User(req.body)
 
-  user.save((err, userInfo) => {
+  user.save((err, user) => {
     if(err) return res.json({success: false, err})
     return res.status(200).json({
       success: true
     })
   })
+})
+
+app.post('/login', (req, res) => {
+  User.findOne({email: req.body.email}, (err, user) => {
+    if(!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다."
+      });
+    }
+
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if(!isMatch)
+        return res.json({loginSuccess: false, message: "비밀번호가 틀렸습니다."});
+
+      user.generateToken((err, user) => {
+        if(err) return res.status(400).send(err);
+
+        res.cookie("x_auth", user.token)
+        .status(200)
+        .json({loginSuccess: true, userId: user._id});
+      });
+    });
+  });
 })
 
 app.listen(port, () => {
